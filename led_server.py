8babed9f8eea49202e7249d48b13fefbc7cbf92e
Devesh Laungani <d7laungani@hotmail.com>
2021-02-16 23:17:19 -0500

    Update led_server.py

--- led_server.py
+++ led_server.py
@@ -9,7 +9,7 @@ import eventlet
 import math
 from flask import Flask, render_template
 
-from neopixel import *
+from rpi_ws281x import Color, PixelStrip, ws
 from socketIO_client import SocketIO, LoggingNamespace
 
 
@@ -21,6 +21,8 @@ LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 REFRESH        = False
+LED_CHANNEL = 0
+LED_STRIP = ws.SK6812_STRIP_RGBW
 
 # Color on led
 R_COLOR = 0
@@ -230,7 +232,7 @@ def shift(sid, data):
 if __name__ == '__main__':
 	# Create NeoPixel object with appropriate configuration.
 	global strip
-	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+    	strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
 	# Intialize the library (must be called once before other functions).
 	strip.begin()
 
